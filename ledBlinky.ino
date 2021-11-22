d2c797172200132ad24b7f293294fc718a241074
dangelo729 <peterdangelo729@gmail.com>
2021-11-22 10:25:59 -0500

    Initial commit

---
+++ ledBlinky.ino
@@ -0,0 +1,20 @@
+
+int ledPin = 22
+             int delayTime = 1000
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+
+  delay(delayTime);
+
+  digitalWrite(ledPin, LOW);
+
+  delay(delayTime);
+
+
+}
